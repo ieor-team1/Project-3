9d6ca26e1e4958d97cb4d8bdda2e66c24ac57fd0
almusanz <almusanz@fulton.ieor.berkeley.edu>
2012-09-18 17:46:52 -0700

    Bah

--- src/Navigator.java
+++ src/Navigator.java
@@ -11,17 +11,18 @@ public class Navigator {
     float trackWidth = 11.4f;
     LightSensor lightSensor = new LightSensor(SensorPort.S4);   
 	DifferentialPilot pilot = new DifferentialPilot(wheelDiameter, trackWidth, Motor.A, Motor.C);
-	
 	ScanRecorder s = new ScanRecorder(Motor.B, lightSensor);
-	double gain = 0.5f;
+	double gain = 1f;
 	public void go(){
+		pilot.setTravelSpeed(30);
 		while(true){
 	      int maxLight = s.scan();
 	      System.out.println("Max Light = " + maxLight + " Angle =" + s._angle1);
-	      if(maxLight>46){
+	      if(maxLight>55){
+	    	  pilot.stop();
 	    	  pilot.rotate(180);
 	      }
-	      pilot.steer(s._angle1*gain);
+	      pilot.steer(-s._angle1*gain);
 	}
 	}
 	
